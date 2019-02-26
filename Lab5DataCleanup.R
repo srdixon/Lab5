d18addb5792faebe5d7c2fb8e95496e61e3d541c
srdixon <sara.dixon1@louisiana.edu>
2019-02-26 14:17:37 -0600

    finished cleaning Peru

--- Lab5DataCleanup.R
+++ Lab5DataCleanup.R
@@ -29,3 +29,29 @@ GOM$Month    = str_pad(GOM$Month, 2, pad = "0")    #pad months with a 0 to make 
 GOM          = na.omit(GOM)                        #clear out NA's
 
 head(GOM)                                          #check that it worked
+
+#CLEANUP PERU
+Peru$month   = str_pad(Peru$month, 2, pad = "0")   #pad months with a 0 to make them 2-digit months
+Peru         = Peru %>% select(-Date)              #delete unneccessary date column
+Peru         = Peru %>% rename(lat  = LAT)         #un-capitalize lat column name
+Peru         = Peru %>% rename(long = LONG)        #"           " long "       "
+Peru         = Peru %>% select(c(long, lat, month, year)) #get rid of random NA columns at end
+
+head(Peru)                                         #check that it worked
+
+#Now convert to decimal degrees
+t   = str_sub(Peru$long, start = -1)
+t1  = str_split_fixed(Peru$long, pattern = "W", n = 3)       #removes W
+t2  = str_split_fixed(string = t1[,1], pattern = "°", n = 2) #split degrees and miutes
+lon = (as.numeric(t2[,2])/60) + as.numeric(t2[,1]) * -1
+
+
+r   = str_sub(Peru$lat, start = -1)
+r1  = str_split_fixed(Peru$lat, pattern = "S", n = 3)        #removes W
+r2  = str_split_fixed(string = r1[,1], pattern = "°", n = 2) #split degrees and miutes
+lat = (as.numeric(r2[,2])/60) + as.numeric(r2[,1]) * -1
+
+Peru$long = lon
+Peru$lat  = lat
+
+head(Peru)
