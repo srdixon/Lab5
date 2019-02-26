f692f047dbd84c4e7159ae9e5239917ebf6309bd
srdixon <sara.dixon1@louisiana.edu>
2019-02-26 14:00:34 -0600

    finished cleanup on Alaska dataset

--- Lab5DataCleanup.R
+++ Lab5DataCleanup.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(tidyr)
 library(dplyr)
+library(stringr)
 
 #SET WORKING DIRECTORY
 setwd("C:/Users/riverdixon/Documents/Grad/Classes/Spring 2019/Spatial Analysis and Management/Lab5")
@@ -14,3 +15,10 @@ setwd("C:/Users/riverdixon/Documents/Grad/Classes/Spring 2019/Spatial Analysis a
 Alaska = read.csv("Alaska.csv", header = TRUE)
 GOM    = read.csv("GOM.csv", header = TRUE)
 Peru   = read.csv("Peru.csv", header = TRUE)
+
+#CLEANUP ALASKA
+Alaska       = Alaska %>% select (-Year) #drop one of the two year columns
+Alaska$month = str_pad(Alaska$month, 2, pad = "0") #pad months with a 0 to make them 2-digit months
+Alaska       = na.omit(Alaska) #clear out NA's
+head(Alaska) #check that it worked
+tail(Alaska) #check that it worked
